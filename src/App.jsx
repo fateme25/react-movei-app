c809d8b3b93abef6872a0f0c1c749da2a7b773b5
Walter Tronics <waltertronics02@gmail.com>
2024-10-19 22:15:26 +0100

    import the home page and display it

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Home from './pages/home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+        <Home />
     </>
   )
 }
