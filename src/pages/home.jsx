fb62d60a688b8f7374df77d8b9b4c001def781b0
fateme <fatemehkamarei99@gmail.com>
2024-10-21 16:04:44 +0330

    testing custom Tailwind classes

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,10 +4,7 @@ function Home() {
 
     return (
         <>
-            <h1>This is the home page</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, totam fugit eveniet maiores accusamus at quasi quod natus illo explicabo deleniti obcaecati, ipsa rem ipsum? Dolores tempore asperiores ipsum facere?</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, totam fugit eveniet maiores accusamus at quasi quod natus illo explicabo deleniti obcaecati, ipsa rem ipsum? Dolores tempore asperiores ipsum facere?</p>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, totam fugit eveniet maiores accusamus at quasi quod natus illo explicabo deleniti obcaecati, ipsa rem ipsum? Dolores tempore asperiores ipsum facere?</p>
+            <h1 className="font-inter text-color-brand-1">This is the home page</h1>
         </>
     )
 }
