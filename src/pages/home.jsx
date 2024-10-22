e27716223c39248bd827dbaca7465e8e0d01d1f1
Fateme kamarei <79019952+fateme25@users.noreply.github.com>
2024-10-22 20:09:24 +0330

    Revert "Fateme"

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,10 +5,12 @@ function Home() {
 
     return (
         <>
+            <Header />
             <h1>This is the home page</h1>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, totam fugit eveniet maiores accusamus at quasi quod natus illo explicabo deleniti obcaecati, ipsa rem ipsum? Dolores tempore asperiores ipsum facere?</p>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, totam fugit eveniet maiores accusamus at quasi quod natus illo explicabo deleniti obcaecati, ipsa rem ipsum? Dolores tempore asperiores ipsum facere?</p>
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic, totam fugit eveniet maiores accusamus at quasi quod natus illo explicabo deleniti obcaecati, ipsa rem ipsum? Dolores tempore asperiores ipsum facere?</p>
+            <Footer />
         </>
     )
 }
