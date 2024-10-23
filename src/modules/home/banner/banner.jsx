c118515d0380472739f6bbb77a8e03a5920fe2e6
Walter Tronics <waltertronics02@gmail.com>
2024-10-23 13:50:03 +0100

    add the banner skeleton frame and import to home

---
+++ src/modules/home/banner/banner.jsx
@@ -0,0 +1,16 @@
+import TopBar from "../../../components/navbars/topBar";
+import banner from '../../../assets/banner.png'
+
+function Banner() {
+
+    return (
+        <>
+            <div className="banner">
+
+            </div>
+        </>
+    )
+}
+
+
+export default Banner
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,24 @@
 import TopBar from "../components/navbars/topBar"
 import Footer from "../components/footer"
+import Banner from "../modules/home/banner/banner"
 
 function Home() {
 
     return (
         <>
+            {/* The topbar component goes here */}
             <TopBar />
-            
+
+            {/* The banner component goes here */}
+            <Banner />
+
+            {/* The coming soon component goes here */}
+
+            {/* The opening this week component goes here */}
+
+            {/* The in theather component goes here */}
+
+            {/* The footer component goes here */}
             <Footer />
         </>
     )
