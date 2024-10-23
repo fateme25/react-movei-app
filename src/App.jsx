0153d88563c902d9cdafb63f13aea0322557b83b
Walter Tronics <waltertronics02@gmail.com>
2024-10-23 10:18:55 +0100

    modify the import link to the styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
-import './App.css'
+import './styles/App.css'
 
 function App() {
 
