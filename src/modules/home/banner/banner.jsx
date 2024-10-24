c22741db34fab6050388e44ad46aa7b089ae2fd1
Walter Tronics <waltertronics02@gmail.com>
2024-10-24 08:19:09 +0100

    add the background image with a shade of black overlay

--- src/modules/home/banner/banner.jsx
+++ src/modules/home/banner/banner.jsx
@@ -5,7 +5,13 @@ function Banner() {
 
     return (
         <>
-            <div className="banner">
+            <div className="banner w-full h-screen relative ">
+
+                <div className='bg absolute w-full h-full '>
+                    <div className="overlay absolute w-full h-full top-0 left-0 bg-[rgba(0, 0, 0, .5)]"></div>
+                    <img src={banner}
+                    className="object-cover w-full h-full" />
+                </div>
 
             </div>
         </>
