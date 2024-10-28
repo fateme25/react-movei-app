37b5efeb0c5681eb944e32585c619e5923f56c15
Walter Tronics <waltertronics02@gmail.com>
2024-10-28 10:35:21 +0100

    add the banner component to the home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,21 +6,24 @@ function Home() {
 
     return (
         <>
-            {/* The topbar component goes here */}
-            <TopBar
-            home={true} />
+            <div className="relative">
+                {/* The topbar component goes here */}
+                <TopBar
+                home={true} />
 
-            {/* The banner component goes here */}
-            <Banner />
+                {/* The banner component goes here */}
+                <Banner />
 
-            {/* The coming soon component goes here */}
+                {/* The coming soon component goes here */}
 
-            {/* The opening this week component goes here */}
+                {/* The opening this week component goes here */}
 
-            {/* The in theather component goes here */}
+                {/* The in theather component goes here */}
 
-            {/* The footer component goes here */}
-            <Footer />
+                {/* The footer component goes here */}
+                <Footer />
+
+            </div>
         </>
     )
 }
