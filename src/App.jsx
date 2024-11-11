7bea67ecd9c4798b3dfc1369dca493a0f23f355a
fateme <fatemehkamarei99@gmail.com>
2024-11-11 18:22:18 +0330

    Configure React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import Home from "./pages/home";
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  });
 
   return (
     <>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </BrowserRouter>
+      </QueryClientProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
