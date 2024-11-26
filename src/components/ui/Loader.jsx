f0841fd58e0a61baf2c3d50301ca07eb601c5b05
fateme <fatemehkamarei99@gmail.com>
2024-11-26 23:28:46 +0330

    add framer motion

--- src/components/ui/Loader.jsx
+++ src/components/ui/Loader.jsx
@@ -1,30 +1,30 @@
 import React, { useEffect, useState } from "react";
+import { motion } from "framer-motion";
 import loader from "../../assets/loadergif.gif";
 import "../../styles/index.css";
-import { motion } from "framer-motion";
 function Loader() {
   const [showLoader, setShowLoader] = useState(true);
   // Set a delay for the loader display
   useEffect(() => {
     const loaderDelay = setTimeout(() => {
       setShowLoader(false);
-    }, 2000); // Set to 2000ms (2 seconds) or your desired delay time
+    }, 5000); // Set to 2000ms (2 seconds) or your desired delay time
 
     return () => clearTimeout(loaderDelay);
   }, []);
-return (
-  showLoader && (
-    <div className="movie_loader">
-      <motion.img
-        src={loader}
-        alt="Loading..."
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-      />
-    </div>
-  )
-);
+  return (
+    showLoader && (
+      <div className="movie_loader">
+        <motion.img
+          src={loader}
+          alt="Loading..."
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.3, delay: 1, ease: "linear" }}
+        />
+      </div>
+    )
+  );
 }
 
 export default Loader;
