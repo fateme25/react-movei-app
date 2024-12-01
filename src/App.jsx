5cc4a438265169e9ca0fbc5a0c18a58c8edf6733
fateme <fatemehkamarei99@gmail.com>
2024-12-01 19:41:44 +0330

    create new routes for authentication

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import Home from "./pages/home";
 import SearchMovieList from "./components/navbars/SearchMovieList";
 import SearchResult from "./components/navbars/SearchResult";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./features/Authentication/Login";
+import Register from "./features/Authentication/Register";
+import ResetPassword from "./features/Authentication/ResetPassword";
 
 function App() {
   const queryClient = new QueryClient({
@@ -18,13 +22,19 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
+        <AuthProvider>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/search" element={<SearchMovieList/>}/>
             <Route path="search/:movieId" element={<SearchResult/>}/>
+            <Route path="login" element={<Login/>}/>
+            <Route path="register" element={<Register/>}/>
+            <Route path="reset-password" element={<ResetPassword/>}/>
           </Routes>
         </BrowserRouter>
+
+        </AuthProvider>
       </QueryClientProvider>
     </>
   );
